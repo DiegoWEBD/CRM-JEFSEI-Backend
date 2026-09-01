54bc89120f6c38a2ffe2ddbb19ad3854073ec4d6
Diego maldonado <diegomz.webdev@gmail.com>
2026-09-01 16:35:42 -0400

    corrección en método de verificación de completitud de información de un prospecto personal

--- app/presentacion/api/prospecto/lib/informacion_completa_prospecto.py
+++ app/presentacion/api/prospecto/lib/informacion_completa_prospecto.py
@@ -6,8 +6,6 @@ def informacion_completa_prospecto(prospecto: Prospecto) -> bool:
     return all([
         prospecto.rut_riesgo is not None,
         prospecto.nombre_riesgo is not None,
-        prospecto.telefono_contacto is not None,
-        prospecto.correo_contacto is not None,
         prospecto.direccion is not None,
         prospecto.region is not None,
         prospecto.comuna is not None
