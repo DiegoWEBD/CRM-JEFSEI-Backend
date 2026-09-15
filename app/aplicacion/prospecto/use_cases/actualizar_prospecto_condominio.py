244815f7e557e06c6fad743e234cbb910485e7b6
Diego maldonado <diegomz.webdev@gmail.com>
2026-09-15 15:01:58 -0300

    corrección bugs en registro o actualización de prospectos al comparar nombre o rut de un prospecto

--- app/aplicacion/prospecto/use_cases/actualizar_prospecto_condominio.py
+++ app/aplicacion/prospecto/use_cases/actualizar_prospecto_condominio.py
@@ -1,8 +1,11 @@
 from app.dominio.administrador_condominio.administrador_condominio import AdministradorCondominio
 from app.dominio.exceptions.recurso_no_encontrado import RecursoNoEncontradoException
+from app.dominio.exceptions.recurso_ya_existe import RecursoYaExisteException
 from app.dominio.exceptions.usuario_no_autorizado import UsuarioNoAutorizadoException
 from app.dominio.linea_negocio.linea_negocio import LineaNegocio
 from app.dominio.prospecto.repositorio_prospectos import RepositorioProspectos
+from app.infraestructura.lib.normalizar_rut import normalizar_rut
+from app.infraestructura.lib.normalizar_texto import normalizar_texto
 
 
 class ActualizarProspectoCondominioUseCase:
@@ -49,6 +52,18 @@ class ActualizarProspectoCondominioUseCase:
         
         if not prospecto.ejecutivo_comercial_asignado or prospecto.ejecutivo_comercial_asignado.rut != rut_usuario:
             raise UsuarioNoAutorizadoException
+
+        if normalizar_texto(prospecto.nombre_riesgo) != normalizar_texto(nombre_riesgo):
+            nombre_existente = self.repositorio_prospectos.buscar_prospecto_por_nombre(nombre_riesgo)
+            if nombre_existente:
+                es_cliente = nombre_existente.id_cliente is not None
+                raise RecursoYaExisteException(f'El {"cliente" if es_cliente else "prospecto"} {nombre_riesgo} ya está registrado')
+
+        if rut_riesgo and normalizar_rut(prospecto.rut_riesgo) != normalizar_rut(rut_riesgo):
+            rut_existente = self.repositorio_prospectos.buscar_prospecto_por_rut(rut_riesgo)
+            if rut_existente:
+                es_cliente = rut_existente.id_cliente is not None
+                raise RecursoYaExisteException(f'El {"cliente" if es_cliente else "prospecto"} {rut_riesgo} ya está registrado')
 
         if id_administrador is not None:
             prospecto.administrador = AdministradorCondominio(
